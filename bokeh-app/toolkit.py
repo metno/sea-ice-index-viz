98ee6799d067f31990a41699b9436d6a3a1b2de4
Michael Yartys <michael.yartys@protonmail.com>
2022-08-11 10:20:21 +0200

    toolkit: change colour of current year
    Set the colour of the line for the current year to black for viridis,
    plasma, and batlow to increase its visibility.
    
    Signed-off-by: Michael Yartys <michael.yartys@protonmail.com>

--- bokeh-app/toolkit.py
+++ bokeh-app/toolkit.py
@@ -176,4 +176,8 @@ def find_line_colours(years, colour):
         colours_in_hex = [matplotlib.colors.to_hex(colour) for colour in colours]
         colour_dict = {year: colour for year, colour in zip(years, colours_in_hex)}
 
+        if colour == ("viridis" or "plasma" or "batlow"):
+            # Set the color of the current year to black.
+            colour_dict[list(years)[-1]] = "#000000"
+
     return colour_dict

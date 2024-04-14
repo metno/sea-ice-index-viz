0ec036abe758b85c00f59efbc1887d96cd134afe
Michael Yartys <mail@yartys.no>
2024-04-14 19:50:00 +0200

    daily: add logos
    Add logos to the lower right corner.
    
    Signed-off-by: Michael Yartys <mail@yartys.no>

--- bokeh-app/daily/main.py
+++ bokeh-app/daily/main.py
@@ -648,9 +648,11 @@ try:
 
 
     def on_load():
-        # Divide the layout into 5 columns. The plot uses 4 columns while the widgets get the last column.
-        gspec[0, 0:4] = pn.pane.Bokeh(plot)
-        gspec[0, 4] = inputs
+        # Divide the layout into 5 rows and 5 columns. The plot uses 5 rows and 4 columns,
+        # the widgets get the last column and first 3 rows, and the logo gets the last 2 rows.
+        gspec[0:5, 0:4] = pn.pane.Bokeh(plot)
+        gspec[0:3, 4] = inputs
+        gspec[3:5, 4] = pn.pane.PNG('/bokeh-app/assets/logo.png', sizing_mode='scale_both')
 
     # There is currently a bug in Bokeh 3.1.1 where it incorrectly calculates the amount of available space. To work
     # around this we load the gridspec elements after the page has finished loading.
